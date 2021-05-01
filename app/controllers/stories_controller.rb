5883aff358a77c102c22f70d91ac3e22b5a79b1b
Fran Cabrera <f.cabrerahiche@yahoo.es>
2021-05-01 17:33:54 -0400

    Autenticación con devise, desafio completo

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -24,7 +24,7 @@ class StoriesController < ApplicationController
   # POST /stories
   # POST /stories.json
   def create
-    @story = Story.new(story_params)
+    @story = Story.new(story_params.merge(user: current_user))
 
     respond_to do |format|
       if @story.save
@@ -40,15 +40,23 @@ class StoriesController < ApplicationController
   # PATCH/PUT /stories/1
   # PATCH/PUT /stories/1.json
   def update
-    respond_to do |format|
-      if @story.update(story_params)
-        format.html { redirect_to @story, notice: 'Story was successfully updated.' }
-        format.json { render :show, status: :ok, location: @story }
-      else
-        format.html { render :edit }
-        format.json { render json: @story.errors, status: :unprocessable_entity }
+    if @story.user == current_user || current_user.admin?
+      respond_to do |format|
+        if @story.update(story_params)
+          format.html { redirect_to @story, notice: 'Story was successfully updated.' }
+          format.json { render :show, status: :ok, location: @story }
+        else
+          format.html { render :edit }
+          format.json { render json: @story.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to stories_url, alert: 'You lack permissions to edit'
     end
+  end
+
+  def mypicstories
+    @current_user_stories = current_user.stories
   end
 
   # DELETE /stories/1
